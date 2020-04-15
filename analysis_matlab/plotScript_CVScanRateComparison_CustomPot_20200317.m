59dc36477f0770fa543f940b1ce8b6b62bfdd156
Hunter Strathman <hstrathman@gmail.com>
2020-04-15 12:17:29 -0600

    Add plot for mean of these in vitro measurements compared to in vivo mean from 20200316.

--- analysis_matlab/plotScript_CVScanRateComparison_CustomPot_20200317.m
+++ analysis_matlab/plotScript_CVScanRateComparison_CustomPot_20200317.m
@@ -18,26 +18,26 @@ outputDir = ['../output/' parts{end}];
 
 %% Plot CV Traces 100mV/s
 load('..\rawData\CustomPot\20200317_TDT22_InVitro_unsureOfSolution\2020-03-17_21hr_03min_51sec_TDT22_CV100.mat');
-[ numTrodes, totalPoints ] = size ( Im );
-scan_length = ceil( totalPoints/2 ); % 2 total scans
+[ numTrodes, totalPoints_vitro ] = size ( Im );
+scan_length_vitro = ceil( totalPoints_vitro/2 ); % 2 total scans
 for ii = 1:numTrodes
     figure(ii)
     [ kk ] = pinoutConverter( 'gamry', 'customPot', 'trodeSpecifier', ii );
-    start_pointer = scan_length; 
-    stop_pointer  = totalPoints;
+    start_pointer = scan_length_vitro; 
+    stop_pointer  = totalPoints_vitro;
     scatter( Vstim( kk, start_pointer:stop_pointer ), ...
              Im( kk, start_pointer:stop_pointer ).*(1e6), '.');
     hold on
 end
 
 load('..\rawData\CustomPot\20200317_TDT22_InVitro_unsureOfSolution\2020-03-17_21hr_05min_10sec_TDT22_CV1000.mat');
-[ numTrodes, totalPoints ] = size ( Im );
-scan_length = ceil( totalPoints / 4 ); % 4 total scans
+[ numTrodes, totalPoints_vitro ] = size ( Im );
+scan_length_vitro = ceil( totalPoints_vitro / 4 ); % 4 total scans
 for ii = 1:numTrodes
     figure(ii)
     [ kk ] = pinoutConverter( 'gamry', 'customPot', 'trodeSpecifier', ii );
-    start_pointer = totalPoints - scan_length; 
-    stop_pointer  = totalPoints;
+    start_pointer = totalPoints_vitro - scan_length_vitro; 
+    stop_pointer  = totalPoints_vitro;
     scatter( Vstim( kk, start_pointer:stop_pointer ), ...
              Im( kk, start_pointer:stop_pointer ).*(1e6), '.');
     title((sprintf('E%02d', ii )))
@@ -49,4 +49,53 @@ for ii = 1:numTrodes
     grid on
 end
 
+%% Rough comparison to in vivo
+% Note: these are not the same arrays, but should give us an idea if there
+% is a "positive trend" in the baseline
+% Tried to do +/- stddev but got way more complicated. Deal with this
+% later. Just plotting means for now. 
+
+% In vitro 100 mV/s
+load('..\rawData\CustomPot\20200317_TDT22_InVitro_unsureOfSolution\2020-03-17_21hr_03min_51sec_TDT22_CV100.mat');
+[ numTrodes, totalPoints_vitro ] = size ( Im );
+scan_length_vitro = ceil( totalPoints_vitro/2 ); % 2 total scans
+% remove bad electrodes for the averaging
+Im( 9, : ) = [];
+ImVitro_mean = mean(Im);
+% ImVitro_std = std(Im);
+start_pointer = scan_length_vitro; 
+stop_pointer  = totalPoints_vitro;
+% mid_pointer = start_pointer + floor(scan_length_vitro/2);
+% ImVitro_std_high = ImVitro_mean(start_pointer:mid_pointer) + ...
+%     ImVitro_std(start_pointer:mid_pointer);
+% ImVitro_std_high = [ImVitro_std_high ...
+%     ImVitro_mean(mid_pointer + 1:stop_pointer) - ...
+%     ImVitro_std(mid_pointer + 1:stop_pointer)];
+% Vstim = Vstim( 1, start_pointer:stop_pointer);
+% 
+figure
+% scatter( Vstim, ...
+%          ImVitro_mean( start_pointer:stop_pointer ).*(1e6), '.');
+% hold on
+scatter( Vstim(1, start_pointer:stop_pointer), ...
+         ImVitro_mean(start_pointer:stop_pointer).*(1e6), '.');
+hold on
+
+% In vivo 100 mV/s
+load('2020-03-16_14hr_04min_14sec_TDT21_CV100.mat');
+% remove bad electrodes for the averaging
+Im( 8, : ) = [];
+Im( end, : ) = [];
+[ numTrodes, totalPoints_vivo ] = size ( Im );
+scan_length_vivo = ceil( totalPoints_vivo/2 ); % 2 total scans
+ImVivo_mean = mean(Im);
+start_pointer = scan_length_vivo; 
+stop_pointer  = totalPoints_vivo;
+scatter( Vstim(1, start_pointer:stop_pointer), ...
+         ImVivo_mean(start_pointer:stop_pointer).*(1e6), '.');
+legend('In vivo', 'In vitro')
+ylim([ -2 3])
+xlim([ -0.2 0.6 ])
+xlabel( 'Potential vs OCP (V)' )
+ylabel( 'Current (uA)' )
 
