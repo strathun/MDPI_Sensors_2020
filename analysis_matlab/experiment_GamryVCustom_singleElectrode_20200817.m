0b13b38617bec49c13d673e91ad6885a2d992cb9
Hunter Strathman <hstrathman@gmail.com>
2020-08-19 18:14:56 -0600

    Improve plots for both scripts. Add 4x4 plots for each electrode and averages. Also include a plot with the original impedance of TDT22 before first change.

--- analysis_matlab/experiment_GamryVCustom_singleElectrode_20200817.m
+++ analysis_matlab/experiment_GamryVCustom_singleElectrode_20200817.m
@@ -52,9 +52,93 @@ xlabel('Frequency (Hz)')
 ylabel('Mag(Impedance)')
 legend('Gamry - R1', 'Gamry - R2', 'Custom')
 title('Single Electrode Comparison')
-
 %%
 % Very strange... Seems to be all over the place
+
+%% Plot Impedance (Gamry vs Custom, single electrode) (Individual)
+figure
+colorArray = lines( 16 );
+kk = 1;
+ll = 2;
+pointerArray = [17 16 15 14 12 13 11 10 9 8 7 6 5 4 3 2]; % Accounts for me accidentally measuring one electrode out of order
+% Vs Gamry Run 1
+for ii = 1:16
+    subplot(4,4,ii)
+    loglog( gamryStructure(kk).f, ...
+            gamryStructure(kk).Zmag, ...
+            'Color', colorArray( ii, : ))
+        hold on
+    % Convert from Tye's pinout to gamry
+    mm = pointerArray(ii);
+    [ jj ] = pinoutConverter( 'gamry', 'customPot', 'trodeSpecifier', ii );
+    loglog( customStructure(mm).f(:,jj), ...
+            customStructure(mm).Zmag(:,jj) , 'o', ...
+            'Color', colorArray( ii, : ))
+    kk = kk + 2;
+    xlim([10 100000])
+    legend('Gamry - R1', 'Custom')
+    grid on
+end
+xlabel('Frequency (Hz)')
+ylabel('Mag(Impedance)')
+
+
+% Vs Gamry Run 2
+figure
+for ii = 1:16
+    subplot(4,4,ii)
+    loglog( gamryStructure(ll).f, ...
+            gamryStructure(ll).Zmag, ...
+            '--', 'Color', colorArray( ii, : ))
+    hold on
+    % Convert from Tye's pinout to gamry
+    mm = pointerArray(ii);
+    [ jj ] = pinoutConverter( 'gamry', 'customPot', 'trodeSpecifier', ii );
+    loglog( customStructure(mm).f(:,jj), ...
+            customStructure(mm).Zmag(:,jj) , 'o', ...
+            'Color', colorArray( ii, : ))
+    ll = ll + 2;
+    xlim([10 100000])
+    legend('Gamry - R2', 'Custom')
+    grid on
+end
+xlabel('Frequency (Hz)')
+ylabel('Mag(Impedance)')
+
+%% Plot Impedance (Gamry vs Custom, single electrode) (Array Averages)
+kk = 1;
+ll = 2;
+pointerArray = [17 16 15 14 12 13 11 10 9 8 7 6 5 4 3 2]; % Accounts for me accidentally measuring one electrode out of order
+for ii = 1:16
+    gamryArray_r1(ii,:) = gamryStructure(kk).Zmag;
+    gamryArray_r2(ii,:) = gamryStructure(ll).Zmag; 
+    % Convert from Tye's pinout to gamry
+    mm = pointerArray(ii);
+    [ jj ] = pinoutConverter( 'gamry', 'customPot', 'trodeSpecifier', ii );
+    customArray(ii,:) =  customStructure(mm).Zmag(:,jj);
+    kk = kk + 2;
+    ll = ll + 2;
+end
+
+gamryAvg_r1 = mean( gamryArray_r1, 1 );
+gamrystd_r1 = std( gamryArray_r1, 1 );
+gamryAvg_r2 = mean( gamryArray_r2, 1 );
+gamrystd_r2 = std( gamryArray_r2, 1 );
+customAvg   = mean( customArray,   1 );
+customstd   = std( customArray,   1 );
+
+figure
+errorbar( gamryStructure(1).f, gamryAvg_r1, gamrystd_r1 );
+hold on
+errorbar( gamryStructure(1).f, gamryAvg_r2, gamrystd_r2 );
+errorbar( customStructure(1).f(:,1), customAvg, customstd );
+
+set(gca,'XScale','log','YScale','log') 
+xlim([10 100000])
+legend('Gamry - R1', 'Gamry - R2', 'Custom')
+grid on
+xlabel('Frequency (Hz)')
+ylabel('Mag(Impedance)')
 
 %% Plot Impedance Gamry, before and after Custom
 figure
@@ -78,14 +162,79 @@ ylabel('Mag(Impedance)')
 legend('Gamry - Before', 'Gamry - After' )
 title('Impedance Changes from Custom Pstat Measurements - Gamry')
 
-%% Plot Impedance (Gamry vs Custom, single electrode)
+%% Plot Impedance Gamry, before and after Custom (Single Electrodes)
 figure
 colorArray = lines( 16 );
-pointerArray1 = [2 3 4 5 6 7 8 9 10 11 13 12 14 15 16 17]; % Accounts for me accidentally measuring one electrode out of order           
+kk = 1;
+ll = 2;
 for ii = 1:16
-    mm = pointerArray(ii);
-    loglog( customStructure(mm).f(:,(mm-1)), ...
-            customStructure(mm).Zmag(:,(mm-1)), ...
+    subplot(4,4,ii)
+    loglog( gamryStructure(kk).f, ...
+            gamryStructure(kk).Zmag, ...
+            'Color', colorArray( ii, : ))
+        hold on
+    loglog( gamryStructure(ll).f, ...
+            gamryStructure(ll).Zmag, ...
+            '--', 'Color', colorArray( ii, : ))
+    kk = kk + 2;
+    ll = ll + 2;
+    xlim([10 100000])
+    grid on
+    legend('Gamry - Before', 'Gamry - After' )
+end
+xlabel('Frequency (Hz)')
+ylabel('Mag(Impedance)')
+
+
+%% Plot Impedance Gamry, before and after Custom (Array Averages)
+% Pulled from above
+figure
+errorbar( gamryStructure(1).f, gamryAvg_r1, gamrystd_r1 );
+hold on
+errorbar( gamryStructure(1).f, gamryAvg_r2, gamrystd_r2 );
+
+set(gca,'XScale','log','YScale','log') 
+xlim([10 100000])
+legend('Gamry - R1', 'Gamry - R2')
+grid on
+xlabel('Frequency (Hz)')
+ylabel('Mag(Impedance)')
+
+%% Plot Phase (Gamry, before and after Custom )(Array Averages)
+kk = 1;
+ll = 2;
+for ii = 1:16
+    gamryPhaseArray_r1(ii,:) = gamryStructure(kk).Phase;
+    gamryPhaseArray_r2(ii,:) = gamryStructure(ll).Phase; 
+    kk = kk + 2;
+    ll = ll + 2;
+end
+
+gamryPhaseAvg_r1 = mean( gamryPhaseArray_r1, 1 );
+gamryPhasestd_r1 = std( gamryPhaseArray_r1, 1 );
+gamryPhaseAvg_r2 = mean( gamryPhaseArray_r2, 1 );
+gamryPhasestd_r2 = std( gamryPhaseArray_r2, 1 );
+
+figure
+errorbar( gamryStructure(1).f, gamryPhaseAvg_r1, gamryPhasestd_r1 );
+hold on
+errorbar( gamryStructure(1).f, gamryPhaseAvg_r2, gamryPhasestd_r2 );
+
+set(gca,'XScale','log','YScale','log') 
+xlim([10 100000])
+legend('Gamry - R1', 'Gamry - R2')
+grid on
+xlabel('Frequency (Hz)')
+ylabel('Phase')
+
+%% Plot Impedance (Custom Single vs Custom Mux, All)
+figure
+colorArray = lines( 16 );
+pointerArray1 = [2 3 4 5 6 7 8 9 10 11 13 12 14 15 16 17]; % Accounts for me accidentally measuring one electrode out of order   
+for ii = 1:16
+    mm = pointerArray1(ii);
+    loglog( customStructure(mm).f(:,(ii)), ...
+            customStructure(mm).Zmag(:,(ii)), ...
             'Color', colorArray( ii, : ))
         hold on
     loglog( customStructure(1).f(:,ii), ...
@@ -97,6 +246,27 @@ xlabel('Frequency (Hz)')
 ylabel('Mag(Impedance)')
 legend('Custom - Single', 'Custom - Mux')
 title('Single Electrode Comparison')
+
+%% Plot Impedance (Custom Single vs Custom Mux, single electrode)
+figure
+colorArray = lines( 16 );
+pointerArray1 = [2 3 4 5 6 7 8 9 10 11 13 12 14 15 16 17]; % Accounts for me accidentally measuring one electrode out of order           
+for ii = 1:16
+    subplot(4,4,ii)
+    mm = pointerArray1(ii);
+    loglog( customStructure(mm).f(:,(ii)), ...
+            customStructure(mm).Zmag(:,(ii)), ...
+            'Color', colorArray( ii, : ))
+        hold on
+    loglog( customStructure(1).f(:,ii), ...
+            customStructure(1).Zmag(:,ii), 'o', ...
+            'Color', colorArray( ii, : ))
+    xlim([100 10000])
+    legend('Custom - Single', 'Custom - Mux')
+    grid on
+end
+xlabel('Frequency (Hz)')
+ylabel('Mag(Impedance)')
 
 %%
 % %% Plot Impedance (Gamry (new HS) vs Mux Pstat)
--- analysis_matlab/plotScript_Gamry_ImpedanceDropAgain_20200814.m
+++ analysis_matlab/plotScript_Gamry_ImpedanceDropAgain_20200814.m
@@ -16,20 +16,24 @@ outputDir = ['../output/' parts{end}];
 [~, ~] = mkdir(outputDir);
 
 %% Extract impedance data
-[gamryStructure_newHS] = ...
+[gamryStructure_Exp1] = ...
     extractImpedanceDataGlobal('..\rawData\Gamry\20200810_TDT22_inVitro_OldPBS_brOutHS');
 
 [gamryStructure_Exp2] = ...
     extractImpedanceDataGlobal('..\rawData\Gamry\20200814_TDT22_InVitro_OldPBS_brOutHS');
 
-%% Plot Impedance (new vs Old HS)
+[gamryStructure_Exp0] = ...
+    extractImpedanceDataGlobal('..\rawData\Gamry\20200323_TDT22_InVitro_OldPBS');
+
+%% Plot Impedance (new vs Old)
 [~, numTraces] = size( gamryStructure_Exp2 );
 figure(1)
 colorArray = lines( numTraces );
 pointerArray = [8 12:25 33 ];
 for ii = 1:numTraces
-    loglog( gamryStructure_newHS(ii).f, ...
-            gamryStructure_newHS(ii).Zmag, ...
+    jj = pointerArray(ii);
+    loglog( gamryStructure_Exp1(jj).f, ...
+            gamryStructure_Exp1(jj).Zmag, ...
             'Color', colorArray( ii, : ))
         hold on
     loglog( gamryStructure_Exp2(ii).f, ...
@@ -40,7 +44,86 @@ xlim([10 100000])
 xlabel('Frequency (Hz)')
 ylabel('Mag(Impedance)')
 legend('Week 1', 'Week 2')
-title('global Impedance Drop (Both Gamry)')
+title('Global Impedance Drop (Both Gamry)')
 
-%%
-%
+%% Plot Impedance (new vs Old) (Individual)
+[~, numTraces] = size( gamryStructure_Exp2 );
+figure
+colorArray = lines( numTraces );
+pointerArray = [8 12:25 33 ];
+for ii = 1:numTraces
+    subplot(4,4,ii)
+    jj = pointerArray(ii);
+    loglog( gamryStructure_Exp1(jj).f, ...
+            gamryStructure_Exp1(jj).Zmag, ...
+            'Color', colorArray( ii, : ))
+        hold on
+    loglog( gamryStructure_Exp2(ii).f, ...
+            gamryStructure_Exp2(ii).Zmag, ...
+            '--', 'Color', colorArray( ii, : ))
+    grid on
+    xlim([10 100000])
+    legend('Week 1', 'Week 2')
+end
+xlabel('Frequency (Hz)')
+ylabel('Mag(Impedance)')
+
+%% Plot Impedance (new vs Old)(Averages)
+[~, numTraces] = size( gamryStructure_Exp2 );
+colorArray = lines( numTraces );
+pointerArray = [8 12:25 33 ];
+for ii = 1:numTraces
+    jj = pointerArray(ii);
+    avgArray_1(ii,:) = gamryStructure_Exp1(jj).Zmag;
+    avgArray_2(ii,:) = gamryStructure_Exp2(ii).Zmag;
+end
+mean_1 = mean( avgArray_1, 1);
+std_1 = std( avgArray_1, 1 );
+mean_2 = mean( avgArray_2, 1);
+std_2 = std( avgArray_2, 1 );
+
+figure
+errorbar( gamryStructure_Exp1(1).f, ...
+        mean_1, std_1 );
+hold on
+errorbar( gamryStructure_Exp2(1).f, ...
+        mean_2, std_2);
+set(gca,'XScale','log','YScale','log')    
+grid on
+xlim([10 100000])
+legend('Week 1', 'Week 2')
+xlabel('Frequency (Hz)')
+ylabel('Mag(Impedance)')
+
+%% Plot Impedance (new vs Old)(Averages) Include original
+[~, numTraces] = size( gamryStructure_Exp2 );
+colorArray = lines( numTraces );
+pointerArray = [8 12:25 33 ];
+for ii = 1:numTraces
+    jj = pointerArray(ii);
+    avgArray_1(ii,:) = gamryStructure_Exp1(jj).Zmag;
+    avgArray_2(ii,:) = gamryStructure_Exp2(ii).Zmag;
+    avgArray_0(ii,:) = gamryStructure_Exp0(ii).Zmag;
+end
+mean_1 = mean( avgArray_1, 1);
+std_1 = std( avgArray_1, 1 );
+mean_2 = mean( avgArray_2, 1);
+std_2 = std( avgArray_2, 1 );
+mean_0 = mean( avgArray_0, 1);
+std_0 = std( avgArray_0, 1 );
+
+figure
+errorbar( gamryStructure_Exp1(1).f, ...
+        mean_1, std_1 );
+hold on
+errorbar( gamryStructure_Exp2(1).f, ...
+        mean_2, std_2);
+errorbar( gamryStructure_Exp0(1).f, ...
+        mean_0, std_0);
+set(gca,'XScale','log','YScale','log')    
+grid on
+xlim([10 100000])
+legend('Week 1', 'Week 2', 'Week 0')
+xlabel('Frequency (Hz)')
+ylabel('Mag(Impedance)')
+
