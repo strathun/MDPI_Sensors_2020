335a491b2c36ca3f2f7f659a55cfcdb2284ccc5f
Hunter Strathman <hstrathman@gmail.com>
2021-03-29 14:47:55 -0600

    Major revision to extractFEMDATA. Had to overhaul to sort all of the new probes in the FEM model. Will NOT WORK with older text files anymore.

--- analysis_matlab/simulation_FEM_16channels_20210323.m
+++ analysis_matlab/simulation_FEM_16channels_20210323.m
@@ -26,9 +26,9 @@ Zest_VCModel = load('..\rawData\Volume-Conductor\20210323_irMonopole_VC_calculat
 
 %% Floating
 numPoints = length(customStructure_floating.f);
-Ref_V_mag = abs(customStructure_floating.Ref_V);
-Gnd_I_mag = abs(customStructure_floating.Gnd_I);
-Zmag_floating = Ref_V_mag./Gnd_I_mag;
+Ref_V_mag_floating = abs(customStructure_floating.Ref_V);
+Gnd_I_mag_floating = abs(customStructure_floating.Gnd_I);
+Zmag_floating = Ref_V_mag_floating./Gnd_I_mag_floating;
 figure(1)
 semilogx(customStructure_floating.f(:,1), Zmag_floating(:,1))
 hold on
@@ -37,15 +37,15 @@ ylabel('mag(Impedance) (Ohms)')
 
 %% Gnded
 numPoints = length(customStructure_gnded.f);
-Ref_V_mag = abs(customStructure_gnded.Ref_V);
-Gnd_I_mag = abs(customStructure_gnded.Gnd_I);
-Zmag_gnd = Ref_V_mag./Gnd_I_mag;
+Ref_V_mag_gnded = abs(customStructure_gnded.Ref_V);
+Gnd_I_mag_gnded = abs(customStructure_gnded.Gnd_I);
+Zmag_gnd = Ref_V_mag_gnded./Gnd_I_mag_gnded;
 figure(1)
 semilogx(customStructure_gnded.f(:,1), Zmag_gnd(:,1))
 xlabel('Frequency (Hz)')
 ylabel('mag(Impedance) (Ohms)')
 legend('Floating', 'Grounded')
-
+title('FEM Impedance; 1 channel (floating) vs 16 parallel (grounded)')
 %%
 % Interesting.... Look like the lower frequencies have a big effect on
 % impedance that drops off at higher frequencies. 
@@ -91,7 +91,7 @@ xlabel('Frequency Markers')
 ylabel('Impedance Magnitude')
 title('1xPBS')
 legend('FEM', 'VC', 'Measured')
-
+title('Impedance for 1 vs 16 channels for all methods')
 %% %Change by freq/model
 
 % Calculate % Change
@@ -122,3 +122,89 @@ legend('FEM', 'VC', 'Measured')
 %%
 % FEM model matches the shape of the % change for the measured, but it is
 % shifted down by about 200%... ish...
+
+%% Plotting changes in current for each method
+figure
+colorArray = lines( 2 );
+% FEM
+semilogx(customStructure_gnded.f(:,1), ...
+         Gnd_I_mag_floating, '*', ...
+         'Color', colorArray( 1, : ), 'LineWidth', 1.5)
+hold on
+semilogx(customStructure_gnded.f(:,1), ...
+         Gnd_I_mag_gnded, '*', ...
+         'Color', colorArray( 2, : ), 'LineWidth', 1.5)
+% % VC
+% semilogx(customStructure_1PBS_inVitro(2).f(:,5), ...
+%          p_Change_VC, 'o', ...
+%          'Color', colorArray( 1, : ), 'LineWidth', 1.5)
+
+% Actual
+semilogx(customStructure_1PBS_inVitro(6).f(:,5), ...
+         customStructure_1PBS_inVitro(2).Vpp_sig_rec(:,5)./4.7e3, ...
+         'Color', colorArray( 1, : ), 'LineWidth', 1.5)
+semilogx(customStructure_1PBS_inVitro(6).f(:,5), ...
+         customStructure_1PBS_inVitro(6).Vpp_sig_rec(:,5)./4.7e3, ...
+         'Color', colorArray( 2, : ), 'LineWidth', 1.5)
+xlabel('Frequency (Hz)')
+ylabel('Current (A)')
+legend('FEM', 'VC', 'Measured')
+title('Current Changes; 1 channel vs 16 parallel')
+%% Looking at estimates for surface voltage changes for measured vs simulated
+% Estimate deltaV for measured values
+I_measured_single = customStructure_1PBS_inVitro(2).Vpp_sig_rec(:,5)./4.7e3;
+Ztot_single       = customStructure_1PBS_inVitro(2).Vpp_ref_rec(:,5)./ ...
+                    I_measured_single;
+Vsurface_single   = I_measured_single .* Ztot_single;
+I_measured_16e    = customStructure_1PBS_inVitro(6).Vpp_sig_rec(:,5)./4.7e3;
+Vsurface_16e      = I_measured_16e .* Ztot_single;
+
+figure
+% Actual
+semilogx( customStructure_1PBS_inVitro(6).f(:,5), ...
+          Vsurface_single, ...
+          'Color', colorArray( 1, : ), 'LineWidth', 1.5)
+hold on
+semilogx( customStructure_1PBS_inVitro(6).f(:,5), ...
+          Vsurface_16e, ...
+          'Color', colorArray( 2, : ), 'LineWidth', 1.5)
+
+%FEM 
+semilogx(customStructure_gnded.f(:,1), ...
+         abs(customStructure_floating.WEOffset_V(:,1)), '*', ...
+         'Color', colorArray( 1, : ), 'LineWidth', 1.5)
+semilogx(customStructure_gnded.f(:,1), ...
+         abs(customStructure_gnded.WEOffset_V(:,1)), '*', ...
+         'Color', colorArray( 2, : ), 'LineWidth', 1.5)
+     
+% REF VOltage for 16e - measured
+semilogx( customStructure_1PBS_inVitro(6).f(:,5), ...
+          customStructure_1PBS_inVitro(6).Vpp_ref_rec(:,5),'--', ...
+          'LineWidth', 1.5)
+xlabel('Frequency (Hz)')
+ylabel('Voltage (V)')
+legend('Vitro - 1e', 'Vitro - 16e', 'FEM - 1e', 'FEM - 16e')
+title('Changes in Surface Voltage')   
+%% Estimated impedance change for FEM based of change in V at surface
+% Calculate estimated delta_Imp
+WE_V_mag_floating = abs(customStructure_floating.WEOffset_V);
+I_est_floating = WE_V_mag_floating./Zmag_floating; % Calculate change in I
+Zmag_floating_WEEst = Ref_V_mag_floating./I_est_floating; % Calculate change in Z
+%
+WE_V_mag_gnded = abs(customStructure_gnded.WEOffset_V);
+I_est_gnded = WE_V_mag_gnded./Zmag_floating; % Calculate change in I
+Zmag_gnded_WEEst = Ref_V_mag_gnded./I_est_gnded; % Calculate change in Z
+%
+delta_Z_est = Zmag_gnded_WEEst - Zmag_floating_WEEst;
+
+% Plot
+figure
+semilogx(customStructure_gnded.f(:,1), ...
+         delta_Z_est)
+hold on
+semilogx(customStructure_gnded.f(:,1), ...
+         (Zmag_gnd - Zmag_floating))
+xlabel('Frequency (Hz)')
+ylabel('Change in Impedance')
+legend('Estimated-FEM', 'Actual-FEM')
+title('Impedance Change Estimated from Change in Surface Voltage (FEM)')
--- matlab/extractFEMData.m
+++ matlab/extractFEMData.m
@@ -39,16 +39,42 @@ for kk = 3:length(fnames)
     % Format data to usable format
     fname = fnames(kk);
     fidi = fopen(fname{1},'r');
-    Data = textscan(fidi, '%f%f%f%f%f%f', 'HeaderLines',5, 'CollectOutput',1);
+    numHeaders = 35;
+    Data = textscan(fidi, '%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f', ...                    
+                   'HeaderLines', 5, 'CollectOutput',1);
     Data = cell2mat(Data);
     fclose(fidi);
+    % Preload blanks
+    dataStructure(kk-2).Ref_V = [];
+    dataStructure(kk-2).WESurface_V = [];
+    dataStructure(kk-2).WE_I = [];
+    dataStructure(kk-2).WEOffset_V = [];
+    dataStructure(kk-2).Gnd_I = [];
+    % Grab data
     dataStructure(kk-2).fname = fname;
     dataStructure(kk-2).f = Data(:,1);
     dataStructure(kk-2).Ref_V = Data(:,2);
-    dataStructure(kk-2).WESurface_V = Data(:,3);
-    dataStructure(kk-2).WE_I = Data(:,4);
-    dataStructure(kk-2).WEOffset_V = Data(:,5);
-    dataStructure(kk-2).Gnd_I = Data(:,6);
+    % Couldn't figure out how to reorganize columns in COMSOL so have to
+    % have hardcoded here. Will ened to update if any additional data is
+    % added.
+    WESurface_V_order   = [ 11 10 09 08 07 03 12 13 ...
+                            14 15 16 17 18 19 20 21];
+    WEOffset_V_order    = [ 26 25 24 23 22 05 27 28 ...
+                            29 30 31 32 33 34 35 36];
+    WE_I_order          = [ 41 40 39 38 37 04 42 43 ...
+                            44 45 46 47 48 49 50 51];
+    Gnd_I_order         = [ 56 55 54 53 52 06 57 58 ...
+                            59 60 61 62 63 64 65 66];
+    for ii = 1:16
+        jj = WESurface_V_order(ii);
+        dataStructure(kk-2).WESurface_V = [dataStructure(kk-2).WESurface_V Data(:,jj)];
+        jj = WE_I_order(ii);
+        dataStructure(kk-2).WE_I = [dataStructure(kk-2).WE_I Data(:,jj)];
+        jj = WEOffset_V_order(ii);
+        dataStructure(kk-2).WEOffset_V = [dataStructure(kk-2).WEOffset_V Data(:,jj)];
+        jj = Gnd_I_order(ii);
+        dataStructure(kk-2).Gnd_I = [dataStructure(kk-2).Gnd_I Data(:,jj)];
+    end
 end
 
 cd(currentFolder)
