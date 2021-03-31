9fd05194c45c346f6d2f43808bb0cb3be7d1c0a8
Hunter Strathman <hstrathman@gmail.com>
2021-03-31 12:33:28 -0600

    New comparison script that uses the updated model (old FEM model had an incorrect equation for the capacitance in the grounded scenario).

--- analysis_matlab/simulation_FEM_16channels_20210329.m
+++ analysis_matlab/simulation_FEM_16channels_20210329.m
@@ -1,7 +1,4 @@
-%% simulation_FEM_16channels_20210323
-% NOTE: This will no longer run. extractFEMData needs to be rolled back to
-% work with the previous shape of the text files. This has been updated as
-% 'simulation_FEM_16channels_20210329'. 
+%% simulation_FEM_16channels_20210329
 % Trying to compare the FEM predicted changes in impedance to those that were
 % actually observed in vitro. 
 
@@ -20,9 +17,9 @@ outputDir = ['../output/' parts{end}];
 
 %% Extract impedance data
 [customStructure_floating] = ...
-    extractFEMData('..\rawData\FEM\raw-data\20210323_Sweep_floating');
+    extractFEMData('..\rawData\FEM\raw-data\20210330_Sweep_floating_40mV');
 [customStructure_gnded] = ...
-    extractFEMData('..\rawData\FEM\raw-data\20210323_Sweep_gnded');
+    extractFEMData('..\rawData\FEM\raw-data\20210330_Sweep_gnded_40mV');
 [customStructure_1PBS_inVitro] = ...
     extractCustomPstatData('..\rawData\CustomPot\20201201_TDT22_InVitro_1xPBS');
 Zest_VCModel = load('..\rawData\Volume-Conductor\20210323_irMonopole_VC_calculation_1xPBS.mat');
@@ -30,7 +27,7 @@ Zest_VCModel = load('..\rawData\Volume-Conductor\20210323_irMonopole_VC_calculat
 %% Floating
 numPoints = length(customStructure_floating.f);
 Ref_V_mag_floating = abs(customStructure_floating.Ref_V);
-Gnd_I_mag_floating = abs(customStructure_floating.Gnd_I);
+Gnd_I_mag_floating = abs(customStructure_floating.Gnd_I(:,5));
 Zmag_floating = Ref_V_mag_floating./Gnd_I_mag_floating;
 figure(1)
 semilogx(customStructure_floating.f(:,1), Zmag_floating(:,1))
@@ -41,14 +38,14 @@ ylabel('mag(Impedance) (Ohms)')
 %% Gnded
 numPoints = length(customStructure_gnded.f);
 Ref_V_mag_gnded = abs(customStructure_gnded.Ref_V);
-Gnd_I_mag_gnded = abs(customStructure_gnded.Gnd_I);
+Gnd_I_mag_gnded = abs(customStructure_gnded.Gnd_I(:,5));
 Zmag_gnd = Ref_V_mag_gnded./Gnd_I_mag_gnded;
 figure(1)
 semilogx(customStructure_gnded.f(:,1), Zmag_gnd(:,1))
 xlabel('Frequency (Hz)')
 ylabel('mag(Impedance) (Ohms)')
 legend('Floating', 'Grounded')
-title('FEM Impedance; 1 channel (floating) vs 16 parallel (grounded)')
+title('FEM; 1 channel (floating) vs 16 parallel (grounded)')
 %%
 % Interesting.... Look like the lower frequencies have a big effect on
 % impedance that drops off at higher frequencies. 
@@ -151,7 +148,7 @@ semilogx(customStructure_1PBS_inVitro(6).f(:,5), ...
          'Color', colorArray( 2, : ), 'LineWidth', 1.5)
 xlabel('Frequency (Hz)')
 ylabel('Current (A)')
-legend('FEM', 'VC', 'Measured')
+legend('FEM-1', 'FEM-16', 'Measured-1', 'Measured-16')
 title('Current Changes; 1 channel vs 16 parallel')
 %% Looking at estimates for surface voltage changes for measured vs simulated
 % Estimate deltaV for measured values
@@ -174,10 +171,10 @@ semilogx( customStructure_1PBS_inVitro(6).f(:,5), ...
 
 %FEM 
 semilogx(customStructure_gnded.f(:,1), ...
-         abs(customStructure_floating.WEOffset_V(:,1)), '*', ...
+         abs(customStructure_floating.WEOffset_V(:,5)), '*', ...
          'Color', colorArray( 1, : ), 'LineWidth', 1.5)
 semilogx(customStructure_gnded.f(:,1), ...
-         abs(customStructure_gnded.WEOffset_V(:,1)), '*', ...
+         abs(customStructure_gnded.WEOffset_V(:,5)), '*', ...
          'Color', colorArray( 2, : ), 'LineWidth', 1.5)
      
 % REF VOltage for 16e - measured
@@ -186,15 +183,14 @@ semilogx( customStructure_1PBS_inVitro(6).f(:,5), ...
           'LineWidth', 1.5)
 xlabel('Frequency (Hz)')
 ylabel('Voltage (V)')
-legend('Vitro - 1e', 'Vitro - 16e', 'FEM - 1e', 'FEM - 16e')
 title('Changes in Surface Voltage')   
-%% Estimated impedance change for FEM based of change in V at surface
+%% Estimated impedance change for FEM based on change in V at surface
 % Calculate estimated delta_Imp
-WE_V_mag_floating = abs(customStructure_floating.WEOffset_V);
+WE_V_mag_floating = abs(customStructure_floating.WEOffset_V(:,5));
 I_est_floating = WE_V_mag_floating./Zmag_floating; % Calculate change in I
 Zmag_floating_WEEst = Ref_V_mag_floating./I_est_floating; % Calculate change in Z
 %
-WE_V_mag_gnded = abs(customStructure_gnded.WEOffset_V);
+WE_V_mag_gnded = abs(customStructure_gnded.WEOffset_V(:,5));
 I_est_gnded = WE_V_mag_gnded./Zmag_floating; % Calculate change in I
 Zmag_gnded_WEEst = Ref_V_mag_gnded./I_est_gnded; % Calculate change in Z
 %
@@ -211,3 +207,59 @@ xlabel('Frequency (Hz)')
 ylabel('Change in Impedance')
 legend('Estimated-FEM', 'Actual-FEM')
 title('Impedance Change Estimated from Change in Surface Voltage (FEM)')
+
+% Estimated vs actual current
+figure
+semilogx(customStructure_gnded.f(:,1), ...
+         Gnd_I_mag_gnded, ...
+         'Color', colorArray( 1, : ), 'LineWidth', 1.5)
+     hold on
+semilogx(customStructure_gnded.f(:,1), ...
+         I_est_gnded, ...
+         'Color', colorArray( 2, : ), 'LineWidth', 1.5)
+%% Current heat maps 
+% NOTE: This is using the currents through the WE surface instead of the
+% Gnd currents used in the calculations above. This is because most gnd
+% currents in the floating case are NaN
+freq = 9; % index of the frequency to be tested. Change as desired
+figure
+str = sprintf('Currents at %d Hz', customStructure_gnded.f(freq,1));
+
+subplot(2,1,1)
+data_11 = abs(customStructure_floating.WE_I(freq,1:8));
+data_22 = abs(customStructure_floating.WE_I(freq,9:16));
+h1 = heatmap([data_11;data_22]);
+title(str)
+subplot(2,1,2)
+data_1 = abs(customStructure_gnded.WE_I(freq,1:8));
+data_2 = abs(customStructure_gnded.WE_I(freq,9:16));
+h2 = heatmap([data_1;data_2]);
+
+%%
+% Plot spectral currents for all electrodes
+figure
+colorArray = lines( 16 );
+for ii = 1:16
+    semilogx(customStructure_floating.f(:,1), ...
+             abs(customStructure_floating.WE_I(:,ii)), 'o', ...
+             'Color', colorArray( ii, : ))
+    hold on
+    semilogx(customStructure_gnded.f(:,1), ...
+             abs(customStructure_gnded.WE_I(:,ii)), '-', ...
+             'Color', colorArray( ii, : ))
+end
+xlabel('Frequency (Hz)')
+ylabel('Current (A)')
+title('Currents for all 16 electrodes; o = 1e, - = 16e')
+
+figure
+% Plot impedance for other electrodes for fun
+for ii = 1:16
+    semilogx(customStructure_gnded.f(:,1), ...
+             Ref_V_mag_gnded./abs(customStructure_gnded.Gnd_I(:,ii)), '-', ...
+             'Color', colorArray( ii, : ))
+    hold on
+end
+xlabel('Frequency (Hz)')
+ylabel('mag(Impedance) (Ohm)')
+title('Impedance of all 16 channels (FEM)')
